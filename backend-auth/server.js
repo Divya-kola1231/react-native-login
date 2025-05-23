29314641a25364c52a1218f660cd465b045d238b
KOLA DIVYA <165469379+Divya-kola1231@users.noreply.github.com>
2025-05-23 18:15:43 +0530

    server.js

--- backend-auth/server.js
+++ backend-auth/server.js
@@ -63,4 +63,6 @@ app.post('/api/login', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on http://0.0.0.0:${PORT}`));
+
+
